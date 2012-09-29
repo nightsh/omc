3386fd9bf0999a016cf6031b89c36cfc1907da44
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2012-09-29 15:24:06 +0300

    Add frustration to uninstall.

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,7 @@
 <?php
 
+// @TODO remove table not working wtf
+
 if( ! defined( ‘ABSPATH’ ) && ! defined( ‘WP_UNINSTALL_ PLUGIN’ ) )
     exit ();
 
@@ -8,6 +10,6 @@ global $wpdb;
 $table_name = $wpdb->prefix . "selfpublisher";
 $sql = "DROP TABLE " . $table_name . ";";
 $wpdb->query($sql);
-require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 db_delta($sql);
 ?>
