86c7a318fca867496c949be6280511d2e251b7b7
Andrei Sebastian Cîmpean <andreiamenta@gmail.com>
2012-09-29 14:55:28 +0300

    Add uninstall to plugin.

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+if( ! defined( ‘ABSPATH’ ) && ! defined( ‘WP_UNINSTALL_ PLUGIN’ ) )
+    exit ();
+
+global $wpdb;
+
+$table_name = $wpdb->prefix . "selfpublisher";
+$sql = "DROP TABLE " . $table_name . ";";
+$wpdb->query($sql);
+require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+db_delta($sql);
+?>
